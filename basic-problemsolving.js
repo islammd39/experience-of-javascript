bcecf3248513ccc8d6c0a0e18571841291fb82ad
islammd39e <islammdrahat308@gmail.comm>
2022-08-10 10:41:22 +0600

    added

---
+++ basic-problemsolving.js
@@ -0,0 +1,26 @@
+
+
+// problem sloving 01 */* find minimum number in array
+
+// const answer =Math.min(...aveNumbers);
+// console.log(answer);
+
+// const aver =(1, 25, 47);
+// const answar = Math.max(aver);
+// console.log(answar);
+
+
+// const aveNumbers =[21, 10, 2, 4, 57, 40, 97, 17, 68];
+// function aver(value,total){
+//      return value+total;
+// }
+// const foo = aveNumbers.reduce(aver);
+// console.log(foo/9);
+
+// function area(num1, num2){
+//     return num1*num2;
+// }
+// const result = area(12,12);
+// console.log(result);
+
+const aveNumbers =[21, 10, 2, 4, 57, 40, 97, 17, 68];
