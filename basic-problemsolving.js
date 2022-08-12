bb18824f9e340f38b3c78cedfefbf4ed133a6dba
islammd39e <islammdrahat308@gmail.comm>
2022-08-12 23:53:37 +0600

    add

--- basic-problemsolving.js
+++ basic-problemsolving.js
@@ -77,3 +77,24 @@
 // }
 // const theNumbar = isPrime(11);
 // console.log(theNumbar);
+
+// function compare(a, b) {
+//     if (a == b) {
+//      return true;
+//     } else {
+//      return false;
+//     }
+//    }
+//    const result = compare(15, '15');
+//    console.log(result);
+
+// function compare(a, b) {
+//     if (a.toString() === b) {
+//      return true;
+//     } else {
+//      return false;
+//     }
+//    }
+//    const result = compare('25', '25');
+//    console.log(result);
+
