272b01d33a9d44915ea0795db39cabc51e75fdc5
islammd39e <islammdrahat308@gmail.comm>
2022-08-13 23:23:00 +0600

    added

---
+++ apand.js
@@ -0,0 +1,45 @@
+
+
+const dollers = document.getElementById('doller');
+const li = document.createElement('li');
+li.innerText = 'top of the world';
+doller.appendChild(li);
+
+
+const mainContainer = document.getElementById('main-container');
+
+const section = document.createElement('section');
+const h1 = document.createElement('h1');
+h1.innerText = 'added a section';
+section.appendChild(h1);
+
+const ul = document.createElement('ul');
+
+const li1 = document.createElement('li');
+li1.innerText = 'frist added text';
+ul.appendChild(li1);
+
+const li2 = document.createElement('li');
+li2.innerText = 'second added text';
+ul.appendChild(li2);
+
+const li3 = document.createElement('li');
+li3.innerText = 'third added text';
+ul.appendChild(li3);
+
+section.appendChild(ul);
+
+mainContainer.appendChild(section);
+
+
+
+const sectionss = document.createElement('section');
+section.innerHTML = `
+<h2>this is backtik grad</h2>
+<ul>
+<li>intertensting</li>
+<li>moja</li>
+<li>wow</li>
+</ul>
+`
+mainContainer.appendChild(sectionss);
--- conect.js
+++ conect.js
@@ -1,1 +1,16 @@
-console.log(document.body);
+// const lists = document.getElementsByClassName('lists');
+// for(const list of lists){
+//     console.log(list.innerText);
+// } 
+
+const sections = document.querySelectorAll('section');
+for(const section of sections){
+    section.style.border = '2px solid red';
+    section.style.marginBottom = '50px';
+    section.style.borderRadius = '10px';
+}
+
+const doller = document.getElementById('doller');
+doller.style.backgroundColor = 'red';
+doller.style.margin = '10px';
+doller.style.borderRadius = '20px';
