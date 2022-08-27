3df71a7567a3ca8931e638c3a2ca97652586f4ac
islammd39e <islammdrahat308@gmail.comm>
2022-08-27 19:32:56 +0600

    file uload via folder

---
+++ problem-sloving.js
@@ -0,0 +1,15 @@
+const students = [
+    {name : 'rahat', age : 30, email: 'rahatislam@gmail.com', avg: 54, fiftyparsent:true},
+    {name : 'raihan', age : 31, email: 'raihan@gmail.com', avg: 52, fiftyparsent:false},
+    {name : 'main', age : 20, email: 'main@gmail.com', avg: 57, fiftyparsent:true},
+    {name : 'rohim', age : 30, email: 'rohim@gmail.com', avg: 44, fiftyparsent:true},
+    {name : 'hasan', age : 30, email: 'hasan@gmail.com', avg: 50, fiftyparsent:true},
+    {name : 'rafsan', age : 30, email: 'rafsan@gmail.com', avg: 54, fiftyparsent:false},
+]
+
+const result = students.filter(student => student.avg >= 50 && student.fiftyparsent === true);
+
+result.map(pro => {
+    const {name,email} =pro;
+    console.log(name)
+})
