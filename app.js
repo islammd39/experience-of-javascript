563f483731d665ca4e7e88133a319090989e9dde
islammd39e <islammdrahat308@gmail.comm>
2022-08-28 10:51:08 +0600

    file uload via folder

---
+++ app.js
@@ -0,0 +1,33 @@
+// const students = [
+//     {name : 'rahat', age : 30, email: 'rahatislam@gmail.com', avg: 54, fiftyparsent:true},
+//     {name : 'raihan', age : 31, email: 'raihan@gmail.com', avg: 52, fiftyparsent:false},
+//     {name : 'main', age : 20, email: 'main@gmail.com', avg: 57, fiftyparsent:true},
+//     {name : 'rohim', age : 30, email: 'rohim@gmail.com', avg: 44, fiftyparsent:true},
+//     {name : 'hasan', age : 30, email: 'hasan@gmail.com', avg: 50, fiftyparsent:true},
+//     {name : 'rafsan', age : 30, email: 'rafsan@gmail.com', avg: 54, fiftyparsent:false},
+// ];
+// const json = JSON.stringify(students)
+
+// console.log(json);
+
+// const shop = {
+//     owner: 'Md rahat islam',
+//     father:'Md jalal islam',
+//     mother:'Mst lili begum',
+//     address:{
+//         street:'tabaria, word-1',
+//         union:'dihi',
+//         post:'shalkona',
+//         policeStation:'sharsha',
+//         division:'khulna',
+//     },
+//     product:['leptop','phone','mouse','keyboard'],
+//     revenue:45000,
+//     isOpen:true,
+//     new:false,
+// };
+
+// const shopjn = JSON.stringify(shop);
+// const phs = JSON.parse(shopjn)
+
+// console.log(phs);
