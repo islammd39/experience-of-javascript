f8b828e5909e44bc89c8aff3a2d99e1b44261da8
islammd39e <islammdrahat308@gmail.comm>
2022-09-02 02:03:40 +0600

    practice

--- this.js
+++ this.js
@@ -1,21 +1,35 @@
+// const students = {
+//     name: 'abbas',
+//     isStudy: true,
+//     job: 5000,
+//     friends: ['jalal', 'rahim', 'lili'],
+//     toDo: {
+//         mornimg: 'breakfast',
+//         noon: 'lunch',
+//         night: 'dinner'
+//     },
+//     time: function () {
+//         return `this.${this.friends} doning ${this.toDo.noon}`
+//     },
+//     teat: function (amount) {
+//         this.job = this.job - amount;
+//         return this.job
+//     }
+// }
+// // const result = 
+// console.log(students.teat(500));
 
-const students = {
-    name : 'abbas',
-    isStudy:true,
-    job:5000,
-    friends:['jalal','rahim','lili'],
-    toDo:{
-       mornimg:'breakfast',
-       noon:'lunch',
-       night:'dinner'
-    },
-    time:function(){
-        return `this.${this.friends} doning ${this.toDo.noon}`
-    },
-    teat:function(amount){
-        this.job = this.job - amount;
-        return this.job
-    }
-}
-// const result = 
-console.log(students.teat(500));
+// const eightynine = () => 89;
+// console.log(eightynine());
+
+// const divive = num => num / 17;
+// console.log(divive(eightynine()));
+
+// const sum = (num1, num2) => {
+//     const res = num1 + num2;
+//     return res / 2};
+// console.log(sum(10,20));
+
+const foo = [54,36,5,8,47,2,3,25,8,69,235,23].map(x => x *7)
+
+console.log(fooJJJ);
